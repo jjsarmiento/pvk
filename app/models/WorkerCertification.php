a9428bb8937e7afdef5baec48d5a396ea98b0f71
jjsarmiento <sarmiento11102@gmail.com>
2016-09-03 15:30:12 +0800

    added model for new table

---
+++ app/models/WorkerCertification.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jan
+ * Date: 9/3/2016
+ * Time: 3:16 PM
+ */
+
+class WorkerCertification extends Eloquent {
+    protected $table = 'worker_certifications';
+    public $timestamps = true;
+}
