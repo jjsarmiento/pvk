491ccadd4a18b3c89d45142f8c9e1ce925995ef6
jjsarmiento <sarmiento11102@gmail.com>
2016-09-12 14:59:44 +0800

    removed username from landing page [GUEST ROLE]

--- app/views/employerhome.blade.php
+++ app/views/employerhome.blade.php
@@ -296,9 +296,11 @@
                                             &nbsp;
                                             {{substr_replace($nw->lastName, str_repeat('*', strlen($nw->lastName)-1), 1)}}
                                         </b><br>
+                                        <!--
                                         <a href="#">{{$nw->username}}</a><br>
                                         <b>Profile Rating:</b> {{($nw->total_profile_progress ? $nw->total_profile_progress : 0)}}%<br>
                                         <a href="/{{$nw->username}}" class="viewSal">View Full Profile</a>
+                                        -->
                                     </span>
                                 </div>
                                 <div style="clear:both;"></div>
